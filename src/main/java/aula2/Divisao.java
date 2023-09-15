e5cc2745cae13c69598ca4c7a8c9a491da8e36e9
AikoJu <julya.aono@sptech.school>
2023-09-15 14:56:44 -0300

    "Aula de algoritimos no WORKOVER"

---
+++ src/main/java/aula2/Divisao.java
@@ -0,0 +1,22 @@
+package aula2;
+
+public class Divisao {
+    public static void main(String[] args) {
+        Integer x ;
+        Integer y ;
+        Integer z = 1 ;
+
+        x  =  15 % 13; //mod
+
+        y = 130/10;//div
+
+        for(int i = 1; i <= 4; i++){
+                z *= 2;
+        };
+
+
+
+
+        System.out.println(x + " " + y + " " + z);
+    }
+}
---
+++ src/main/java/aula3/BuiltIN.java
@@ -0,0 +1,24 @@
+package aula3;
+
+
+import java.util.Random;
+
+public class BuiltIN {
+    public static void main(String[] args) {
+
+        //1)
+        Double x = Math. sqrt(9) + Math.sqrt(100);
+        //2)
+        Double y = Math.PI * 12;
+        //3)Integer Z = Math.random(20);
+
+        //4)
+        String a = "brasil".toUpperCase();
+
+        //5)
+        String b = "casa";
+        int b2 = b.length();
+
+        System.out.println(x + " " +  y + " " + a + " " + b2);
+    }
+}
